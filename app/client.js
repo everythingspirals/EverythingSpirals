138cc2ceb07f72c66e1ed6a3394abac64beebffe
Dushan Perera <dushan.v.perera@gmail.com>
2015-08-15 23:45:55 -0700

    less ego

--- app/client.js
+++ app/client.js
@@ -10,13 +10,23 @@ app.config(function($stateProvider, $urlRouterProvider) {
     .state('portfolio', {
       url: "/portfolio",
       controller:"PortfolioCtrl",
-      templateUrl: "/modules/portfolio/client/portfolio-view.html"
+      templateUrl: "/modules/portfolio/client/portfolio-view.html",
+      data: {
+        img:"portfolio.jpg"
+      }
     })
     .state('blog', {
       url: "/blog",
       controller:"BlogCtrl",
-      templateUrl: "/modules/blog/client/blog-view.html"
+      templateUrl: "/modules/blog/client/blog-view.html",
+      data: {
+        img:"blog.jpg"
+      }
     });
+});
+
+app.controller('NavCtrl', function($scope, $state){
+	$scope.state = $state;
 });
 
 app.filter('trust', ['$sce', function($sce){
